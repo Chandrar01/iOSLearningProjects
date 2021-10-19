ebf9491ad2d1e5400b52a91c9d5b1165be4b050e
Chandrashekhar <shekharrangnekar@gmail.com>
2021-10-19 14:45:44 -0700

    Hello World Project First Committed to the Git Repository from XCODE

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Chandrashekhar Rangnekar on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
